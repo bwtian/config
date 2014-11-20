4b864c7a2b2d446a934ed6caff97b1f1d2726038
tian <bwtian@gmail.com>
2014-11-20 15:41:27 +0900

    / ‘shells/bash/setBash.sh’

--- shells/bash/setBash.sh
+++ shells/bash/setBash.sh
@@ -6,4 +6,4 @@ cp ~/.bash_aliases ~/.bash_aliases.backup.`date +%Y-%m-%d-%H:%M`
 rm -f ~/.bash_aliases
 rm -f ~/.bashrc
 ln -sfv ~/SparkleShare/config/shells/bash/bashrc ~/.bashrc
-ln -sfv ~/SparkleShare/config/shells/bash/bash_aliases ~/.bash_aliases
+ln -sfv ~/SparkleShare/config/shells/bash/bash_aliases.sh ~/.bash_aliases
