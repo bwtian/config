6ce5c9180953189b2974e205567d7305641b4764
tian <bwtian@gmail.com>
2014-11-20 15:45:18 +0900

    / ‘shells/bash/setBash.sh’

--- shells/bash/setBash.sh
+++ shells/bash/setBash.sh
@@ -2,8 +2,8 @@
 ### This file is automaticlly created by linuxInstall.org, do not change!!!
 #!/bin/bash
 cp ~/.bashrc ~/.bashrc.backup.`date +%Y-%m-%d-%H:%M`
-cp ~/.bash_aliases ~/.bash_aliases.backup.`date +%Y-%m-%d-%H:%M`
-rm -f ~/.bash_aliases
+#cp ~/.bash_aliases ~/.bash_aliases.backup.`date +%Y-%m-%d-%H:%M`
+#rm -f ~/.bash_aliases
 rm -f ~/.bashrc
 ln -sfv ~/SparkleShare/config/shells/bash/bashrc ~/.bashrc
 ln -sfv ~/SparkleShare/config/shells/bash/bash_aliases.sh ~/.bash_aliases
