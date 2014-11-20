eec430644c1d343f347e234f442e1c1d1feba570
tian <bwtian@gmail.com>
2014-11-20 15:34:40 +0900

    + ‘shells/bash/bash_path.sh’ + ‘shells/bash/bashrc’ + ‘shells/bash/setBash.sh’

---
+++ shells/bash/setBash.sh
@@ -0,0 +1,9 @@
+
+### This file is automaticlly created by linuxInstall.org, do not change!!!
+#!/bin/bash
+cp ~/.bashrc ~/.bashrc.backup.`date +%Y-%m-%d-%H:%M`
+cp ~/.bash_aliases ~/.bash_aliases.backup.`date +%Y-%m-%d-%H:%M`
+rm -f ~/.bash_aliases
+rm -f ~/.bashrc
+ln -s ~/Dropbox/config/shells/bash/bashrc ~/.bashrc
+ln -s ~/Dropbox/config/shells/bash/bash_aliases ~/.bash_aliases
