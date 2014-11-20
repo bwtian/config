85b47e3c93affc29ade207935b0730610fa3bc26
tian <bwtian@gmail.com>
2014-11-20 15:35:14 +0900

    / ‘shells/bash/setBash.sh’

--- shells/bash/setBash.sh
+++ shells/bash/setBash.sh
@@ -5,5 +5,5 @@ cp ~/.bashrc ~/.bashrc.backup.`date +%Y-%m-%d-%H:%M`
 cp ~/.bash_aliases ~/.bash_aliases.backup.`date +%Y-%m-%d-%H:%M`
 rm -f ~/.bash_aliases
 rm -f ~/.bashrc
-ln -s ~/Dropbox/config/shells/bash/bashrc ~/.bashrc
-ln -s ~/Dropbox/config/shells/bash/bash_aliases ~/.bash_aliases
+ln -sfv ~/SparkleShare/config/shells/bash/bashrc ~/.bashrc
+ln -sfv ~/SparkleShare/config/shells/bash/bash_aliases ~/.bash_aliases
