85dbd6bfdcc1058091f27566050ecba9220421d7
tian <bwtian@gmail.com>
2014-11-24 15:37:06 +0900

    / ‘xmodmap/setXmodmap.sh’ / ‘xmodmap/xintrc’

--- xmodmap/setXmodmap.sh
+++ xmodmap/setXmodmap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ln -sfv xmodmap ~/.xmodmap
+ln -sfv ~/SparkleShare/config/xmodmap/Xmodmap ~/.Xmodmap
+ln -sfv ~/SparkleShare/config/xmodmap/xinitrc ~/.xinitrc
 # in bashrc
 # source ~/SparkleShare/config/xmodmap/xmodmap
  
