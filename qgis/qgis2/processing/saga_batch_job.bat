cc5ed0d04e68dcbcdf8a022d9e9a1ff3438c4807
lab1 <bwtian@gmail.com>
2014-11-27 11:39:02 +0900

    / ‘qgis/qgis2/processing/saga_batch_job.bat’ / ‘qgis/qgis2/python/plugins/processing/algs/saga/ext/__init__.pyc’

--- qgis/qgis2/processing/saga_batch_job.bat
+++ qgis/qgis2/processing/saga_batch_job.bat
@@ -1,5 +1,5 @@
-set SAGA=C:/PROGRA~1/QGISCH~1/apps\saga
-set SAGA_MLB=C:/PROGRA~1/QGISCH~1/apps\saga\modules
+set SAGA=C:/PROGRA~1/QGISBR~1/apps\saga
+set SAGA_MLB=C:/PROGRA~1/QGISBR~1/apps\saga\modules
 PATH=PATH;%SAGA%;%SAGA_MLB%
-saga_cmd shapes_polygons "Polygon Centroids" -POLYGONS "C:/Users/phd/.qgis2/python/plugins\processing\tests\data\polygons.shp" -CENTROIDS "C:\Users\phd\AppData\Local\Temp\processing\2945c6f008444c8b968f4ba0dfe651b0\CENTROIDS.shp"
+saga_cmd shapes_polygons "Polygon Centroids" -POLYGONS "C:/Users/QGIS/.qgis2/python/plugins\processing\tests\data\polygons.shp" -CENTROIDS "C:\Users\QGIS\AppData\Local\Temp\processing\b3ea6d19297c431898ba164855eb90e2\CENTROIDS.shp"
 exit
