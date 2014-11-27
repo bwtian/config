e7cb15f876c4e22d728d689c76925400f4acc5a8
lab1 <bwtian@gmail.com>
2014-11-27 11:52:26 +0900

    / ‘qgis/qgis2/processing/saga_batch_job.bat’

--- qgis/qgis2/processing/saga_batch_job.bat
+++ qgis/qgis2/processing/saga_batch_job.bat
@@ -1,5 +1,5 @@
 set SAGA=C:/PROGRA~1/QGISBR~1/apps\saga
 set SAGA_MLB=C:/PROGRA~1/QGISBR~1/apps\saga\modules
 PATH=PATH;%SAGA%;%SAGA_MLB%
-saga_cmd shapes_lines "Line-Polygon Intersection" -LINES "D:/tian/greenTuff/greenTuffL.shp" -POLYGONS "D:/tian/greenTuff/hkdLand_141127_113019.shp" -METHOD 0 -INTERSECT "C:\Users\QGIS\AppData\Local\Temp\processing\fd4870907b004967b3fca5dbf3e647c1\INTERSECT.shp"
+saga_cmd shapes_tools "Merge Shapes Layers" -MAIN "C:\Users\QGIS\AppData\Local\Temp\processing\796f9fea265543c1a5f9ab5cbcbbafd7\OUTPUT.shp" -LAYERS "D:/tian/greenTuff/greenTuffL.shp" -OUT "C:\Users\QGIS\AppData\Local\Temp\processing\c002371f3b1e452bb87d945c70f381fa\OUT.shp"
 exit
