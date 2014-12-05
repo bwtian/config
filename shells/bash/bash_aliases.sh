37ab333654f593f381cb7e7e302732e270c901c9
tian <bwtian@gmail.com>
2014-12-06 07:02:58 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -73,7 +73,7 @@ abiword --to=doc ${1%\.pdf}-img.html
 }
 function zippdf(){
 inFile=$1
-outFile
+outFile= ${1%\.pdf}-compressed
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=foo-compressed.pdf foo.pdf
 }
 function mvt(){
