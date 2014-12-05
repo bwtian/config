d575b9d3b934b3fa78e0d0454a23c34b6bad6405
tian <bwtian@gmail.com>
2014-12-06 06:50:11 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -64,7 +64,7 @@ alias r='rstudio'
 alias web='chromium-browser'
 alias men='mendeleydesktop'
 function tex2txt(){
- detex -n  $1 ${1%\.tex}my_paper/project.tex   > out.txt
+ detex -n  $1 ${1%\.tex}_paper/project.tex   > out.txt
 }
 function pdf2html(){
 pdftohtml -noframes -q -p -c $1 ${1%\.pdf}-img.html
