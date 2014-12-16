7894e490ca8c90d48d4bd9dc3f1f34e6ef2743a7
tian <bwtian@gmail.com>
2014-12-16 14:57:48 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -66,7 +66,7 @@ alias men='mendeleydesktop'
 #### Tex and PDF
 function pdf2html(){
 pdftohtml -noframes -q -p -c $1 ${1%\.pdf}-img.html
-abiword --to=doc ${1%\.pdf}-img.html
+abiword --to=doc ${1%\.pdf}.html
 }
 function pdfCompress(){
 #gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${1%\.pdf}-compressed.pdf $1
