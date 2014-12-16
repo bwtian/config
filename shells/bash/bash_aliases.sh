853fda5aa8877d37bd6cc1fbc3ac2778518d1d6c
tian <bwtian@gmail.com>
2014-12-16 14:56:45 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -86,7 +86,7 @@ function texWc(){
 dir=build
 echo "World counts by detex and wc lines, words, characters:" 
 detex -n  $1 | sed '/^\s*$/d' | wc
-echo "World counts by detex and wc lines, words, charactersr:" 
+echo "World counts by texcount.pl html:" 
 texcount.pl -html -inc -incbib $filename.html
 }
 
