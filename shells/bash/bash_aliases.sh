c3550b6e38be95a7221416166898c18489f7dc00
tian <bwtian@gmail.com>
2014-12-22 18:07:12 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -90,10 +90,10 @@ texcount $1
 
 function texLink(){
     unalias ln
-linkfrom=~/SparkleShare/phdtex/Contents
+linkfrom=~/SparkleShare/phdtex/Contents/
 linkto=$1
 #files=$(ls -lR $linkfrom)
-files=$(find . -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2-)
+files=$(find $linkfrom -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2-)
 for i in $files; do ln -sfv $i $1; done 
 }
 function htmlWc(){
