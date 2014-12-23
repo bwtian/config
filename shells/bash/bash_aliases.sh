db054f049b4de14ebff1b595e09188ebfea1347e
tian <bwtian@gmail.com>
2014-12-23 12:42:27 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -147,7 +147,7 @@ pdflatex -synctex=1 -interaction=nonstopmode -output-directory=$dir $filename.te
 function texBB(){
 mkdir -p build
 dir=build
-filename=${1:-main}%\.tex
+filename=${1:-main}
 echo "Compiling your Tex to build folder...please wait...!"
 texClean $filename
 pdflatex -synctex=1 -interaction=nonstopmode -output-directory=$dir $filename.tex
@@ -167,7 +167,7 @@ okular $dir/$filename.pdf
 function texB(){
 mkdir -p build
 dir=build
-filename=${1:-main.tex}
+filename=${1%\.tex:-main.tex%\.tex}
 echo "Compiling your Tex to build folder...please wait...!"
 texClean $filename
 pdflatex -synctex=1 -interaction=nonstopmode -output-directory=$dir $filename.tex
