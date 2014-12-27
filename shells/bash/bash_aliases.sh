fbbd00f8963b81d5ae419067e558e35b78cb2e36
tian <bwtian@gmail.com>
2014-12-27 11:58:52 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -348,7 +348,7 @@ tClearup(){
     tex=("*.tex$" "*.Rnw$")
     doc="*.doc *.docx *.ppt *.pptx *.htm *.html"
     pic="*.png *.jpg *.jpeg *.bmp *.gif *.svg"
-    sta="*.sas *.dat *.csv"
+    sta="*.sas *.dat *.csv *.txt *.Rds *.Rdata"
     vecData="*.xml *.kml *.kmz *.tiff *.nc *.shp *.e00, *.csv *.xls *.xlsx "
     grdData="*.tif *.tiff *.img *.dem *.geotiff "
     zip="*.zip *.7z *.tar *.rar *.gz"
