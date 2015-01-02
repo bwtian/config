bb8ca6087fadb68cf00e8221909783e561000fd8
tian <bwtian@gmail.com>
2015-01-02 19:07:14 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -93,7 +93,7 @@ texcount $1
 function texLink(){
     unalias ln
 linkfrom=${2:-~/SparkleShare/phdtex/thesis/}
-linkto=$1
+linkto=${1}
 #files=$(ls -lR $linkfrom)
 files=$(find $linkfrom -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2-)
 for i in $files; do ln -sfv $i $1; done 
