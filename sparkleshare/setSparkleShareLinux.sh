6b4fe83fbecf3da77a941e92cd896004ecd8fd28
tian <bwtian@gmail.com>
2015-01-08 11:10:17 +0900

    + ‘sparkleshare/Windows/’ / ‘sparkleshare/setSparkleShareLinux.sh’

--- sparkleshare/setSparkleShareLinux.sh
+++ sparkleshare/setSparkleShareLinux.sh
@@ -1,1 +1,2 @@
-\ln -sfv ~/SparkleShare/config/sparkleshare/configLinux.xml ~/.config/sparkleshare/config.xml 
+#\ln -sfv ~/SparkleShare/config/sparkleshare/configLinux.xml ~/.config/sparkleshare/config.xml 
+cp -i ~/SparkleShare/config/sparkleshare/Linux/config.xml ~/.config/sparkleshare/config.xml 
