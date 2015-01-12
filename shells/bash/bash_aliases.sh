490c74f478235a5a5765c23fa2393957232c8d18
tian <bwtian@gmail.com>
2015-01-12 21:41:30 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -181,6 +181,27 @@ echo "Success!"
 cp  $dir/$filename.pdf .
 okular $filename.pdf
 }
+function texE(){
+mkdir -p build
+dir=build
+filename=${1:-main}
+echo "Compiling your Tex to build folder...please wait...!"
+texClean $filename
+pdflatex -synctex=1 -interaction=nonstopmode -output-directory=$dir $filename.tex
+for i in `ls $dir/*.aux`; do bibtex $i; done
+#bibtex     $dir/$filename.aux
+        makeindex  $dir/$filename.aux
+        makeindex  $dir/$filename.idx
+        makeglossaries -d $dir $filename
+        makeindex  $dir/$filename.nlo -s  $dir/nomencl.ist -o  $dir/$filename.nls
+pdflatex -synctex=1 -interaction=nonstopmode -output-directory=$dir $filename.tex
+        makeindex  $dir/$filename.nlo -s  $dir/nomencl.ist -o  $dir/$filename.nls
+pdflatex -synctex=1 -interaction=nonstopmode -output-directory=$dir $filename.tex
+echo "Success!"
+# evince $dir/$filename.pdf
+cp  $dir/$filename.pdf .
+okular $filename.pdf
+}
 function texB(){
 mkdir -p build
 dir=build
