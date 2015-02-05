0bffcf20c31bf3e4b4ce1ae8fb16e124a9806991
tian <bwtian@gmail.com>
2015-02-05 12:36:53 +0900

    / ‘shells/bash/bash_aliases.sh’

--- shells/bash/bash_aliases.sh
+++ shells/bash/bash_aliases.sh
@@ -386,7 +386,7 @@ function p2pdf(){
     for i in $pic; do convert $i $(basename -s $i).pdf; done
 }
 function pdf2png(){
-    pdf=$(1:-*.pdf)
+    pdf="*.pdf"
     for i in $pdf; do convert $i $(basename $i .pdf).png; done
 }
 tClearup(){
