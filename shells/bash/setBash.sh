547992ea1199f2c0d8311f7418251dab21a2fa42
t <bwtian@gmail.com>
2018-03-07 00:26:10 +0800

    / ‘shells/bash/setBash.sh’

--- shells/bash/setBash.sh
+++ shells/bash/setBash.sh
@@ -5,5 +5,5 @@ cp ~/.bashrc ~/.bashrc.backup.`date +%Y-%m-%d-%H:%M`
 #cp ~/.bash_aliases ~/.bash_aliases.backup.`date +%Y-%m-%d-%H:%M`
 #rm -f ~/.bash_aliases
 rm -f ~/.bashrc
-ln -sfv ~/SparkleShare/config/shells/bash/bashrc ~/.bashrc
+ln -sfv ~/SparkleShare/linconfig/shells/bash/bashrc ~/.bashrc
 #ln -sfv ~/SparkleShare/config/shells/bash/bash_aliases.sh ~/.bash_aliases
